957283c39f2b207e983631a400e938e106a61bcf
AliMyr <rabo41ali@gmail.com>
2025-02-21 20:15:08 +0500

    (ADD) Idle Anim, Run Anim

--- Assets/Scripts/Components/PlayerController.cs
+++ Assets/Scripts/Components/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     private IMovable movable;
     private IDamageable damageable;
     private IInputService inputService;
+    private Animator animator;
 
     // —корость поворота в градусах в секунду
     public float rotationSpeed = 720f;
@@ -19,34 +20,29 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         movable = GetComponent<IMovable>();
-        if (movable == null)
-            Debug.LogError("IMovable отсутствует на " + gameObject.name);
 
         damageable = GetComponent<IDamageable>();
-        if (damageable == null)
-            Debug.LogError("IDamageable отсутствует на " + gameObject.name);
+
+        animator = GetComponentInChildren<Animator>();
+
     }
 
     private void Update()
     {
         // ѕолучаем направление ввода через DI-сервис
         Vector3 inputDirection = inputService.GetInputDirection();
-
+        Vector3 moveDir = inputDirection.normalized;
         if (inputDirection.sqrMagnitude > 0.01f)
         {
-            // Ќормализуем направление движени€
-            Vector3 moveDir = inputDirection.normalized;
-
-            // ƒвигаем персонажа
+            
             movable.Move(moveDir);
 
-            // ¬ычисл€ем целевой поворот на основе направлени€ движени€
             Quaternion targetRotation = Quaternion.LookRotation(moveDir, Vector3.up);
-            // ѕлавно поворачиваем текущую ориентацию в сторону целевого поворота
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
 
-        // ѕример: нажали клавишу H дл€ нанесени€ урона (тестовый вызов)
+        animator.SetFloat("Speed", moveDir.magnitude, 0.1f, Time.deltaTime);
+
         if (Input.GetKeyDown(KeyCode.H))
         {
             damageable.TakeDamage(10);
